442edb0fd34e30fcddb8046688f370621a9ce7e4
Yararibas15 <yara.faustino@escola.pr.gov.br>
2026-03-24 11:47:52 -0300

    prova

---
+++ ex10.js
@@ -0,0 +1,8 @@
+//Verifique se um número é igual a 100
+let numero = Number(prompt("Digite um número:"));
+
+if (numero === 100) {
+    console.log("É igual a 100");
+} else {
+    console.log("Não é igual a 100");
+}
---
+++ ex11.js
@@ -0,0 +1,8 @@
+//Verifique se um nmero é divisivel por 3
+let numero = Number(prompt("Digite um número:"));
+
+if (numero % 3 === 0) {
+    console.log("É divisível por 3");
+} else {
+    console.log("Não é divisível por 3");
+}
